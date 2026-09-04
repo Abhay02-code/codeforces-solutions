783ce89679bc1460055793e5997fcb5b27bb8c78
Abhay Kumar Aditya <abhayaditya305@gmail.com>
2026-09-04 19:29:50 +0530

    Solve Codeforces 1206B

--- B_Make_Product_Equal_One.cpp
+++ B_Make_Product_Equal_One.cpp
@@ -1,29 +1,40 @@
-#include<bits/stdc++.h>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main() {
     int n;
-    cin>>n;
-    vector<int>a(n);
-    for(int i = 0; i < n; i++){
-        cin>>a[i];
+    cin >> n;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
+
     int ans = 0;
     int negative = 0;
-    for(int i = 0; i < n; i++){
-        if(a[i]>0){
-            ans += a[i]-1;
+    int zero = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        if (a[i] > 0) {
+            ans += a[i] - 1;
         }
-        else if(a[i]<0){
-            ans += abs(a[i]+1);
+        else if (a[i] < 0) {
+            ans += abs(a[i] + 1);
             negative++;
         }
-        else{
+        else {
             ans += 1;
+            zero++;
         }
-
     }
-    if(negative%2 != 0) ans += 2;
+
+    if (negative % 2 != 0 && zero == 0) {
+        ans += 2;
+    }
+
     cout << ans << endl;
+
     return 0;
 }
